9ac3c8cd80ee5faf14de1ca8c724cb629c075c9b
Josh <38544454+JoshuaScript@users.noreply.github.com>
2018-10-16 19:57:56 -0500

    removed unused code

--- test/Home.test.js
+++ test/Home.test.js
@@ -3,9 +3,6 @@ const { By, Builder, until, Key } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const path = require('chromedriver').path;
 
-// const service = new  chrome.ServiceBuilder(path).build();
-// chrome.setDefaultService(service);
-
 const driver = new Builder().forBrowser('chrome').build();
 
 describe('The homepage', function() {
